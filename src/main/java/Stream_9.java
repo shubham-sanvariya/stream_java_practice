24ae8c49280c8faa7151443f511c0ece1d29350f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-10-25 15:52:09 +0530

    // find the kth longest word

--- src/main/java/Stream_9.java
+++ src/main/java/Stream_9.java
@@ -8,5 +8,10 @@ public static void main(String[] args) {
     List<String> words = Arrays.asList("cat", "elephant", "dog", "giraffe", "zebra");
     String res = words.stream().max(Comparator.comparing(String::length)).orElse("");
     System.out.println(res);
+
+    // find the kth longest word
+    int k = 2 - 1; // -1 because of indexing
+    String kLong = words.stream().sorted(Comparator.comparing(String::length).reversed()).skip(k).toList().getFirst();
+    System.out.println(kLong);
 }
 }
