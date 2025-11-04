066848bde771bd33a3b20e0962691d0857e25b2d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-11-04 23:36:23 +0530

    Find Maximum using reduce

--- src/main/java/Stream_21.java
+++ src/main/java/Stream_21.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Stream_21 {
 //     Sum of numbers
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(5, 10, 15, 20);
+        List<Integer> numbers = Arrays.asList(7, 2, 10, 4);
         int sum = numbers.stream().reduce(0,(acc, cur) -> {
                 acc += cur;
                 return acc;
@@ -12,5 +12,14 @@ public class Stream_21 {
         });
 
         System.out.println(sum);
+
+//        Find Maximum using reduce
+
+        int max = numbers.stream().reduce(0,(acc, cur) -> {
+           if (acc < cur) acc = cur;
+           return acc;
+        });
+
+        System.out.println(max);
     }
 }
