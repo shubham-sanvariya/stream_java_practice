77c586bb113012879fda1c58a2118b93a7c5cb32
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-11-08 15:15:28 +0530

    Find students from Hyderabad with a grade greater than 8.0 && Find the student with the highest grade

---
+++ src/main/java/Stream_26.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Stream_26 {
+    public static void main(String[] args) {
+
+        List<Student> students = Arrays.asList(
+                new Student("Rahul", "Sharma", "Hyderabad", 8.38, 19, "Civil"),
+                new Student("Amit", "Verma", "Delhi", 8.4, 21, "IT"),
+                new Student("Suresh", "Reddy", "Chennai", 7.5, 20, "Civil"),
+                new Student("Kiran", "Patel", "Mumbai", 9.1, 20, "IT"),
+                new Student("Arjun", "Naidu", "Bengaluru", 7.83, 20, "Civil")
+        );
+
+//        1. Find students from Hyderabad with a grade greater than 8.0
+            List<String> resHy = students.stream().filter(s -> s.city.equals("Hyderabad") && s.grade > 8.0).map(Student::Firstname).toList();
+        System.out.println(resHy);
+//        2. Find the student with the highest grade
+            Student resHG = students.stream().max(Comparator.comparingDouble(Student::grade)).orElse(null);
+        System.out.println(resHG);
+//        3. Count the number of students in each department
+//        4. Find the average grade per department
+//        5. List students sorted by age and then by grade
+//        6. Create a comma-separated list of student names
+//        7. Check if all students are above 18
+//        8. Find the department with the most students
+//        9. Divide students into those who have grades above 8.0 and below
+//        10. Find the student with the longest full name
+
+
+    }
+
+    record Student(
+            String  Firstname, String  lastname, String  city, double  grade, int age , String  department
+    ){}
+}
